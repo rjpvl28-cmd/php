c06c5de284642b8f18dd5f4c551da884e33846fc
itschool <itschool@tou.edu.kz>
2026-02-14 11:55:27 +0500

    1

---
+++ mysql.fe/src/pages/HomePage.vue
@@ -0,0 +1,25 @@
+<script> 
+    import axios from 'axios';
+    export default {
+        data() {
+            return {
+                title: '',
+            }
+        },
+        methods: {
+            async loadTitle() {
+                const result = await axios.get('http://mysql.be/index.php')
+                this.title = result.data.title
+            }
+        }
+    }
+</script>
+
+<template>
+   {{ title }}
+   <button
+        @click="loadTitle"
+   >Загрузить данные</button>
+</template>
+
+<style scoped></style>
---
+++ mysql.fe/src/router/index.js
@@ -0,0 +1,15 @@
+import HomePage from '@/pages/HomePage.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: HomePage,
+      name: 'home'
+    }
+  ],
+})
+
+export default router
