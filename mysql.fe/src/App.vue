97cd3e919cb0a1125d795093ec2d741a10a89294
itschool <itschool@tou.edu.kz>
2026-02-15 09:18:59 +0500

    1

--- mysql.fe/src/App.vue
+++ mysql.fe/src/App.vue
@@ -1,7 +1,7 @@
-<script></script>
-
 <template>
   <router-view />
 </template>
 
-<style scoped></style>
+<script>
+
+</script>
--- mysql.fe/src/pages/HomePage.vue
+++ mysql.fe/src/pages/HomePage.vue
@@ -1,25 +1,23 @@
-<script> 
-    import axios from 'axios';
-    export default {
-        data() {
-            return {
-                title: '',
-            }
-        },
-        methods: {
-            async loadTitle() {
-                const result = await axios.get('http://mysql.be/index.php')
-                this.title = result.data.title
-            }
-        }
-    }
-</script>
-
 <template>
-   {{ title }}
-   <button
-        @click="loadTitle"
-   >Загрузить данные</button>
+{{ title }}
+<button
+@click="loadTitle"
+>Загрузить данные</button>
 </template>
 
-<style scoped></style>
+<script>
+import axios from 'axios';
+export default {
+data() {
+    return {
+        title: '',
+    }
+},
+methods: {
+    async loadTitle() {
+        const result = await axios.get('http://mysql.be/index.php')
+        this.title = result.data.title
+    }
+}
+}
+</script>
