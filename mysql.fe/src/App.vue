a7a27f6e8a741b2d605d70127be920f4c2bc9c40
itschool <itschool@tou.edu.kz>
2026-04-26 09:14:16 +0500

    1

--- mysql.fe/src/App.vue
+++ mysql.fe/src/App.vue
@@ -1,7 +1,23 @@
 <template>
-  <router-view />
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <router-link
+        :to="{name: 'main'}"
+        class="navbar-brand">
+        Portal
+      </router-link>
+    </div>
+  </nav>
+
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <script>
+export default {
 
+}
 </script>
+
+<style scoped></style>
